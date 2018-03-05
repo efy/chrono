11cda5a8760377d24706929e50ac65d3a294e63a
Shaun Dowling <shaun.dowling@gmail.com>
2018-03-05 21:11:48 +0000

    logOpts and commit types and skeleton log function

--- main.go
+++ main.go
@@ -4,21 +4,52 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 var (
-	// will be set by goreleaser to release tag when building
-	version = "master"
+	// version will be set by goreleaser to release tag when building
+	v = "master"
 
 	// command options
-	v = flag.Bool("v", false, "print the current version")
+	author  = flag.String("author", "", "the commit author")
+	version = flag.Bool("v", false, "print the current version")
 )
 
 func main() {
 	flag.Parse()
 
-	if *v {
-		fmt.Println(version)
+	if *version {
+		fmt.Println(v)
 		os.Exit(0)
 	}
+
+	if *author == "" {
+		fmt.Println("author must be provided")
+		os.Exit(1)
+	}
+
+	opts := logOpts{
+		author: *author,
+	}
+
+	_, err := log(opts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+type logOpts struct {
+	author string
+}
+
+type commit struct {
+	date time.Time
+}
+
+// Call git log with format pretty and parse into commits
+func log(opts logOpts) ([]commit, error) {
+	var commits []commit
+	return commits, nil
 }
